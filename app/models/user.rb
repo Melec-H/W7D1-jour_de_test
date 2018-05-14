de838fb016865b20120e87875ea91aff448bf29b
du halgouet <duhalgouetm@yahoo.com>
2018-05-14 21:10:31 +0200

    second commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+
 end
